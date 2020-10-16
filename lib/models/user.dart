cf22e696e9502a061a673fda242a9b608dfe7c23
JesAmplified <67679328+JesAmplified@users.noreply.github.com>
2020-10-17 00:45:45 +0800

    article app

---
+++ lib/models/user.dart
@@ -0,0 +1,14 @@
+class User {
+  final String uid;
+
+  User({this.uid});
+}
+
+class UserInfo {
+  final String uid;
+  final String firstname;
+  final String imageUrl;
+  final String lastname;
+
+  UserInfo({this.uid, this.firstname, this.imageUrl, this.lastname});
+}
